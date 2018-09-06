27aa93cf11e37bf7313595f5b31b8f14645890e0
alexthayn <alexthayn>
2018-09-06 17:43:53 -0600

    adding files

---
+++ BloomFilter_AThayn/BloomFilter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloomFilter_AThayn
+{
+    class BloomFilter
+    {
+        //Max number of values in bitmap
+        static int MAX = 999999999;
+        bool[] _wordBitmap = new bool[MAX];
+
+        //Initialize bloom filter with the worlist.txt file
+        public BloomFilter()
+        {
+            Console.WriteLine("Loading dictionary bitmap with values...");
+            ReadDictionary();
+        }
+
+        //Read in the dictionary and store hashes in bitmap
+        private void ReadDictionary()
+        {
+            System.IO.StreamReader wordlist = new System.IO.StreamReader("wordlist.txt");
+            String line;
+
+            while((line = wordlist.ReadLine()) != null)
+            {
+                _wordBitmap[CalculateWordHash(line)] = true;
+            }
+        }
+
+        //Calculate a has for any string given
+        public int CalculateWordHash(String word)
+        {
+            word = word.ToLower();
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(word);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            int hashInt = Math.Abs(BitConverter.ToInt32(hash, 0));
+            hashInt = hashInt % MAX;
+            return hashInt;
+        }
+
+        //Check to see if string is in the dictionary
+        public bool CheckWordInDictionary(String word)
+        {
+            int hash = CalculateWordHash(word);
+
+            if (_wordBitmap[hash] == true)
+                return true;
+            return false;
+        }
+    }
+}
--- BloomFilter_AThayn/Program.cs
+++ BloomFilter_AThayn/Program.cs
@@ -10,6 +10,23 @@ namespace BloomFilter_AThayn
     {
         static void Main(string[] args)
         {
+            BloomFilter bloomFilter = new BloomFilter();
+            String userInput;
+            String continueProgram;
+
+            do
+            {
+                System.Console.WriteLine("Enter a word to check: ");
+                userInput = Console.ReadLine();
+                if (bloomFilter.CheckWordInDictionary(userInput) == true)
+                    Console.WriteLine(userInput + " is contained in the loaded word list.");
+                else
+                    Console.WriteLine(userInput + " is not contained in the loaded word list.");
+
+                Console.WriteLine("Would you like to check another word? (Y/N)");
+                continueProgram = Console.ReadLine();
+            } while (continueProgram.ToLower().StartsWith("y"));
+
         }
     }
 }
