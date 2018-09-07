fb72a38328bf60f50d0eb922e829df44aaeb44d7
alexthayn <alexthayn>
2018-09-07 17:23:55 -0600

    added support for more than one hash to be calculated and stored

--- BloomFilter_AThayn/BloomFilter.cs
+++ BloomFilter_AThayn/BloomFilter.cs
@@ -10,7 +10,7 @@ namespace BloomFilter_AThayn
     class BloomFilter
     {
         //Max number of values in bitmap
-        static int MAX = 999999999;
+        static int MAX = 9999999;
         bool[] _wordBitmap = new bool[MAX];
 
         //Initialize bloom filter with the worlist.txt file
@@ -28,29 +28,49 @@ namespace BloomFilter_AThayn
 
             while((line = wordlist.ReadLine()) != null)
             {
-                _wordBitmap[CalculateWordHash(line)] = true;
+                int[] hashValues = CalculateWordHash(line);
+                _wordBitmap[hashValues[0]] = true;
+                _wordBitmap[hashValues[1]] = true;
+                _wordBitmap[hashValues[2]] = true;
+                _wordBitmap[hashValues[3]] = true;
             }
         }
 
-        //Calculate a has for any string given
-        public int CalculateWordHash(String word)
+        //Calculate two hash values for any string given
+        public int[] CalculateWordHash(String word)
         {
             word = word.ToLower();
             MD5 md5 = MD5.Create();
-            byte[] inputBytes = Encoding.ASCII.GetBytes(word);
-            byte[] hash = md5.ComputeHash(inputBytes);
+            SHA256 sHA256 = SHA256.Create();
 
-            int hashInt = Math.Abs(BitConverter.ToInt32(hash, 0));
-            hashInt = hashInt % MAX;
-            return hashInt;
+            byte[] inputBytes = Encoding.ASCII.GetBytes(word);
+            byte[] md5Hash = md5.ComputeHash(inputBytes);
+            byte[] sha256Hash = sHA256.ComputeHash(inputBytes);
+
+            //Split md5 byte array into two segments
+            byte[] firstHash = md5Hash.Take((md5Hash.Length + 1) / 2).ToArray();
+            byte[] secondHash = md5Hash.Skip((md5Hash.Length + 1) / 2).ToArray();
+
+            //Split sha256 byte array into two sub arrays
+            byte[] thirdHash = sha256Hash.Take((sha256Hash.Length + 1) / 2).ToArray();
+            byte[] fourthHash = sha256Hash.Skip((sha256Hash.Length + 1) / 2).ToArray();
+
+            //Set four values to return for each string passed in
+            int firstValue = Math.Abs(BitConverter.ToInt32(firstHash, 0)) % MAX;
+            int secondvalue = Math.Abs(BitConverter.ToInt32(secondHash, 0)) % MAX;
+            int thirdValue = Math.Abs(BitConverter.ToInt32(thirdHash, 0)) % (MAX/4);
+            int fouthValue = Math.Abs(BitConverter.ToInt32(fourthHash, 0)) % (MAX/8);
+
+            return new int[] { firstValue, secondvalue, thirdValue, fouthValue };
         }
 
         //Check to see if string is in the dictionary
         public bool CheckWordInDictionary(String word)
         {
-            int hash = CalculateWordHash(word);
+            int [] hash = CalculateWordHash(word);
 
-            if (_wordBitmap[hash] == true)
+            if (_wordBitmap[hash[0]] == true && _wordBitmap[hash[1]] == true &&
+                _wordBitmap[hash[2]] == true && _wordBitmap[3] == true)
                 return true;
             return false;
         }
